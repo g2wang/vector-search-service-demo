4f0eac0e3b70a9186e53a3b292193f1c7f511b48
Guangde Wang <g2wang@gmail.com>
2025-02-18 02:28:08 -0500

    removed files not in use; added README.txt for files downloaed from HuggingFace

---
+++ all-MiniLM-L6-v2/README.txt
@@ -0,0 +1,2 @@
+Files in this directory are downloaded from HuggingFace:
+https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/tree/main
--- query_tip.sh
+++ query_tip.sh
@@ -2,5 +2,5 @@
 
 curl -X POST http://localhost:3000/search \
   -H "Content-Type: application/json" \
-  -d '{"text": "Recipes for Slow Cooker Chicken"}'
+  -d '{"text": "Mars planet"}'
 
